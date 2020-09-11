9d8c30326655e1423a1537fb2b259d05660abd92
jisoo <2802749246@qq.com>
2020-09-11 13:10:37 +0800

    增加Person

---
+++ src/main/java/org/javagod/Person.java
@@ -0,0 +1,15 @@
+package org.javagod;
+
+import java.util.List;
+
+/**
+ * @Classname Person
+ * @Description TODO
+ * @Date 2020/9/11 12:53
+ * @Created by admin
+ */
+public class Person {
+    private String name;
+    private int head;
+    private int age;
+}
